254e0a60c1c22056faefa327aacbd5fcfd680d2b
Rajagopal S <rjg.sanka@gmail.com>
2020-06-13 23:07:38 +0800

    Language

---
+++ src/bird/characteristics/Language.java
@@ -0,0 +1,16 @@
+package bird.characteristics;
+
+public enum Language {
+	ENGLISH("Cock-a-doodle-doo"),
+	DANISH("kykyliky");
+	
+	String sound;
+	
+	Language(String sound) {
+		this.sound=sound;
+	}
+
+	public String getSound() {
+		return sound;
+	}
+}
